49972e9a12c266a8db756c88b6249b4ee2584eea
Bryant Spicer <konviction@gmail.com>
2025-03-21 09:59:16 -0400

    added test for cleaninput function

---
+++ repl_test.go
@@ -0,0 +1,52 @@
+package main
+
+import "testing"
+
+func TestCleanInput(t *testing.T) {
+	cases := []struct {
+		input    string
+		expected []string
+	}{
+		{
+			input:    "  hello  world  ",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input:    "hello world",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input:    "HELLO WORLD",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input:    "Hello World ",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input:    "",
+			expected: []string{},
+		},
+	}
+	for _, c := range cases {
+		actual := cleanInput(c.input)
+		// Check the length of the actual slice against the expected slice
+		// if they don't match, use t.Errorf to print an error message
+		// and fail the test
+		if len(actual) != len(c.expected) {
+			t.Errorf("slice length missmatch A: %v E: %v", len(actual), len(c.expected))
+			t.Fail()
+		}
+		for i := range actual {
+			word := actual[i]
+			expectedWord := c.expected[i]
+			// Check each word in the slice
+			// if they don't match, use t.Errorf to print an error message
+			// and fail the test
+			if word != expectedWord {
+				t.Errorf("words in slice dont match. A: %s E: %s", word, expectedWord)
+				t.Fail()
+			}
+		}
+	}
+}
