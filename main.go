06a14061c65c889d8c1088ede1aef9024e7d2e8d
Bryant Spicer <konviction@gmail.com>
2025-04-09 12:15:08 -0400

    Implimented CleanInput Function

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
+import "strings"
+
 func cleanInput(text string) []string {
-	return []string{}
+	splitFn := func(c rune) bool { return c == ' ' }
+	output := strings.FieldsFunc(strings.ToLower(text), splitFn)
+	for i := range len(output) - 1 {
+		output[i] = strings.TrimSpace(output[i])
+	}
+	return output
 }
 
 func main() {
