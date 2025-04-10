e051c108058ab0ac1aaebb3f6aa1b7fa39082b9e
Bryant Spicer <konviction@gmail.com>
2025-04-10 13:33:40 -0400

    refactored and moved logic to repl.go file

---
+++ repl.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func cleanInput(text string) []string {
+	splitFn := func(c rune) bool { return c == ' ' }
+	output := strings.FieldsFunc(strings.ToLower(text), splitFn)
+	for i := range len(output) - 1 {
+		output[i] = strings.TrimSpace(output[i])
+	}
+	return output
+}
+
+func startRepl() {
+	//scanner to read from standard input
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+
+		//wait for user input (enter key pressed)
+		if !scanner.Scan() {
+			// if scan fails exit loop
+			fmt.Println("\nExiting Pokedex...")
+			break
+		}
+
+		// get and clean input text
+		input := cleanInput(scanner.Text())
+
+		// skip if empty
+		if len(input) == 0 {
+			continue
+		}
+
+		// split and get first word
+		command := input[0]
+
+		// print command
+		fmt.Printf("Your command was: %s\n", command)
+	}
+}
