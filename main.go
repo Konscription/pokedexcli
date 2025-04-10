b2c32c2fe00f3771432a38d621c4103237aa77c8
Bryant Spicer <konviction@gmail.com>
2025-04-10 13:28:27 -0400

    added command entry logic to main loop

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
-import "strings"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func cleanInput(text string) []string {
 	splitFn := func(c rune) bool { return c == ' ' }
@@ -12,5 +17,31 @@ func cleanInput(text string) []string {
 }
 
 func main() {
+	//scanner to read from standard input
+	scanner := bufio.NewScanner(os.Stdin)
 
+	for {
+		fmt.Print("Pokedex > ")
+
+		//wait for user input (enter key pressed)
+		if !scanner.Scan() {
+			// if scan fails exit loop
+			fmt.Println("\nExiting Pokedex...")
+			break
+		}
+
+		// get and clean input text
+		input := cleanInput(scanner.Text())
+
+		// skip if empty
+		if len(input) == 0 {
+			continue
+		}
+
+		// split and get first word
+		command := input[0]
+
+		// print command
+		fmt.Printf("Your command was: %s\n", command)
+	}
 }
